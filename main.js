53ffbebb7cb39dbafaf827e6adc33b5bccf49ef8
andreykeycee <tessarianisbest@gmail.com>
2018-09-19 19:38:36 +0300

    basic things

---
+++ main.js
@@ -0,0 +1,10 @@
+(function() {
+  const taskDiv = document.getElementsByClassName('task-div');
+  const addButton = document.getElementsByClassName('add-button');
+  const input = document.getElementById('new-task');
+  const newTask = document.getElementById('new-task').value;
+
+  let addTask = function() {
+    
+  }
+})
