3883489f9fe1f2d5c33ac6e33d79b45030b7aabb
andreykeycee <tessarianisbest@gmail.com>
2018-09-20 19:53:55 +0300

    add tasks is working!

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
-(function() {
-  const taskDiv = document.getElementsByClassName('task-div');
-  const addButton = document.getElementsByClassName('add-button');
-  const input = document.getElementById('new-task');
-  const newTask = document.getElementById('new-task').value;
+window.onload = function(){
+  const input = document.getElementById('task-input');
+  const addButton = document.getElementById('add-button');
+  const taskList = document.getElementById('task-list');
 
-  let addTask = function() {
-    
+
+  function addTask() {
+    if(input.value === '') {
+      alert('please insert your task');
+    } else {
+      const task = document.createElement('li');
+      const checkbox = document.createElement('input');
+      const span = document.createElement('span');
+      checkbox.type = 'checkbox';
+      span.textContent = input.value;
+      task.appendChild(checkbox);
+      task.appendChild(span);
+      taskList.appendChild(task);
+      input.value = '';
+    }
   }
-})
+
+  addButton.addEventListener('click', addTask);
+
+  // able to add new task with enter
+  input.addEventListener('keydown', function(e) {
+    if(e.keyCode === 13) {
+      // set enter to not submiting my form
+      e.preventDefault();
+      addTask();
+    }
+  });
+}
