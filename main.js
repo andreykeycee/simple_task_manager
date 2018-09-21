bb7b3adcc4e457136500167b366378f3c29062fa
andreykeycee <tessarianisbest@gmail.com>
2018-09-21 20:19:20 +0300

    All basic things is working!

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-window.onload = function(){
+window.onload = function() {
   const input = document.getElementById('task-input');
   const addButton = document.getElementById('add-button');
   const taskList = document.getElementById('task-list');
-
+  const taskDiv = document.getElementById('task-div');
 
   function addTask() {
     if(input.value === '') {
@@ -11,13 +11,38 @@ window.onload = function(){
       const task = document.createElement('li');
       const checkbox = document.createElement('input');
       const span = document.createElement('span');
+
       checkbox.type = 'checkbox';
       span.textContent = input.value;
       task.appendChild(checkbox);
       task.appendChild(span);
+      // check existance of delete button
+      let existDeleteButton = document.getElementById('delete-button');
+      // if button not exist - create it
+      if(existDeleteButton === null) {
+        addDeleteButton();
+      }
       taskList.appendChild(task);
+      // crossline over completed task
+      checkbox.addEventListener('click', function() {
+        if(span.className === '') {
+          span.className = 'crossline';
+        } else {
+          span.className = '';
+        }
+      });
       input.value = '';
     }
+  }
+
+  function addDeleteButton() {
+    const deleteButton = document.createElement('button');
+    deleteButton.id = 'delete-button';
+    deleteButton.textContent = 'Clear';
+    deleteButton.addEventListener('click', function() {
+      taskList.innerHTML = '';
+    })
+    taskDiv.appendChild(deleteButton);
   }
 
   addButton.addEventListener('click', addTask);
